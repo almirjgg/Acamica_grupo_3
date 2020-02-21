6e270a1fcd87cbd57d677fbec2164189f9fd06bc
almir garcia <almirjgg@gmail.com>
2020-02-20 21:51:44 -0300

    main
    main

---
+++ js/main.js
@@ -0,0 +1,59 @@
+//Actividad de funciones
+
+
+function main() {
+    //
+    var pass = prompt("COLOCA LA CONTRASEÑA PARA INGRESAR A LA APLICACION").toLocaleLowerCase();
+    if (pass == "luis") {
+        var menu = "Operaciones disponibles\n\n";
+        menu += "SUMA\n";
+        menu += "PROMEDIO\n";
+        menu += "PERIMETRO\n";
+        var opcion = prompt(menu.toLocaleLowerCase());
+
+        if (opcion == "suma") {
+            document.write("la suma de los numeros es: " + suma());
+        } else if (opcion == "promedio") {
+            document.write("El promedio de los numeros es: " + promedio());
+        } else if (opcion == "perimetro") {
+            document.write("El perimetro es: " + perimetro());
+        } else {
+            alert("la opcion no es correcta");
+        }
+
+    } else {
+        alert("CLAVE INCORRECTA NO PUEDES INGRESAR A LA APP");
+    }
+
+}
+
+function suma(num1, num2) {
+    num1 = parseInt(prompt("Ingrese el primer numero"));
+    num2 = parseInt(prompt("Ingrese el segundo numero"));
+    var res = num1 + num2;
+
+    return res;
+
+}
+
+function promedio(num1, num2, num3, num4, num5) {
+    num1 = parseInt(prompt("Ingrese el primer numero"));
+    num2 = parseInt(prompt("Ingrese el segundo numero"));
+    num3 = parseInt(prompt("Ingrese el tercer numero"));
+    num4 = parseInt(prompt("Ingrese el cuarto numero"));
+    num5 = parseInt(prompt("Ingrese el quinto numero"));
+
+    var promedio = (num1 + num2 + num3 + num4 + num5) / 5;
+
+    return promedio;
+
+}
+
+function perimetro(diametro) {
+    diametro = parseInt(prompt("ingrese el diametro del circulo para calcular el perimetro"));
+    var perimetro = Math.PI * diametro;
+    return perimetro;
+
+}
+
+main();
